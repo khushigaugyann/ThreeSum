9d933866b6faba8fee0308a7d1449f20bf670bf0
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-29 14:32:49 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new List<>();
+        // Sort the array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    // Found a triplet with zero sum
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Skip duplicate elements for j
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    // Skip duplicate elements for k
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    // Move the pointers
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    // Sum is less than zero, increment j to increase the sum
+                    j++;
+                } else {
+                    // Sum is greater than zero, decrement k to decrease the sum
+                    k--;
+                }
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        int[] myIntArray = {1, 2, 3, 4, 5};
+        List<List<Integer>> a = threeSum(myIntArray);
+    }
+}
